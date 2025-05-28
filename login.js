1c24717fbf4cb80e28f5a87d7e692af3c789b56a
brunooliveiraalmeida <brunnolindo05@gmail.com>
2025-05-28 12:46:45 -0300

    login.js

---
+++ login.js
@@ -0,0 +1,28 @@
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+const form = document.querySelector('.login-form');
+
+
+const validateInput = ({target}) => {
+
+  if (target.value.length > 2)
+  {
+    button.removeAttribute('disabled');
+    return;
+  }
+  
+    button.setAttribute('disabled', '');
+  
+
+}
+ const handlesubmit = (event) => {
+    event.preventDefault();
+
+    localStorage.setItem('player', input.value);
+    window.location = 'jogo da memoria.html'
+    
+ }
+
+input.addEventListener('input', validateInput);
+form.addEventListener('submit', handlesubmit);
+
